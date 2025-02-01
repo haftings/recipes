b809c349d4db149684c9d84bfdf1a755b5e3ee71
Phillip Haftings <phillip.haftings@example.com>
2025-02-01 09:33:20 -0500

    Stir Fry, Pepper Beef and Cabbage

---
+++ Stir Fry, Pepper Beef and Cabbage.md.m
@@ -0,0 +1,28 @@
+# Stir Fry, Pepper Beef and Cabbage
+
+Rice
+- 1.5 cups **water**
+- 0.5 tsp **salt**
+- 1 Tbsp **butter**
+- 1 cup white **rice** *(dry)*
+
+Main Dish
+- 2 Tbsp **olive oil**
+- 4 cloves **garlic** *(minced)*
+- 0.5 lb **ground beef**
+- 0.5 small head **cabbage** *(shredded)*
+- 1 **red bell pepper** *(thinly sliced)*
+- 2 Tbsp **soy sauce**
+- 1 tsp **cornstarch**
+- 0.5 cup cold **water**
+- ~ pinch **salt** *(to taste)*
+- ~ 1 tsp **pepper** *(to taste)*
+
+Instructions
+1. Sauté **garlic** with **oil** in large skillet over **med-high** heat for just **5 sec**
+1. Add **ground beef**, stir-fry to brown **5 min - 7 min**
+1. Drain excess fat
+1. Add **cabbage** and **pepper**, cook until tender
+1. Stir in **soy sauce**, **salt**, and **pepper**
+1. Mix **cornstarch**, **water** in small dish, stir into pan
+1. Cook and stir until thickened
